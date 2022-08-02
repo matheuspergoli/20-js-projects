324886df6d27a467bd9fe9130773f1ddb8f93f88
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-08-02 15:07:39 -0300

    Project 5 - DOM Array Methods

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -41,6 +41,13 @@ function sortByRichest() {
   updateDOM()
 }
 
+// Filter only millionaires
+function showMillionaires() {
+  data = data.filter(user => user.money > 1000000)
+
+  updateDOM()
+}
+
 // Add new object to data array
 function addData(obj) {
   data.push(obj)
@@ -70,3 +77,4 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser)
 doubleBtn.addEventListener('click', doubleMoney)
 sortBtn.addEventListener('click', sortByRichest)
+showMillionairesBtn.addEventListener('click', showMillionaires)
