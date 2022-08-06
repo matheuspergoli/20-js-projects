af1b6ceeb3f9469430a83349eb4a14721b41a118
Matheus Pergoli <matheus.pergoli2015@yahoo.com>
2022-08-06 15:58:15 -0300

    Project 7 - Hangman Game

--- hangman-game/script.js
+++ hangman-game/script.js
@@ -1,6 +1,6 @@
 const wordElement = document.getElementById('word')
 const wrongLettersElement = document.getElementById('wrong-letters')
-const playAgainBtn = document.getElementById('play-again')
+const playAgainBtn = document.getElementById('play-button')
 const popup = document.getElementById('popup-container')
 const notification = document.getElementById('notification-container')
 const finalMessage = document.getElementById('final-message')
@@ -32,7 +32,28 @@ function displayWord() {
 
 // Update the wrong letters
 function updateWrongLettersElement() {
+  //Display wrong letters
+  wrongLettersElement.innerHTML = `
+    ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+    ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `
 
+  // Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length
+
+    if (index < errors) {
+      part.style.display = 'block'
+    } else {
+      part.style.display = 'none'
+    }
+  })
+
+  // Check if lost
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost'
+    popup.style.display = 'flex'
+  }
 }
 
 // Show notification
@@ -63,6 +84,20 @@ window.addEventListener('keydown', event => {
       }
     }
   }
+})
+
+// Restart game and play again
+playAgainBtn.addEventListener('click', () => {
+  // Empty arrays
+  correctLetters.splice(0)
+  wrongLetters.splice(0)
+
+  selectedWord = words[Math.floor(Math.random() * words.length)]
+  displayWord()
+
+  updateWrongLettersElement()
+
+  popup.style.display = 'none'
 })
 
 displayWord()
